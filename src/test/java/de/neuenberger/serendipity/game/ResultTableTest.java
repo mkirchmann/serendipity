1d8c0ce2263f1305e6e1e03f8ca135c66481890d
Michael Kirchmann <ext-michael.kirchmann@here.com>
2016-06-15 09:53:01 +0200

    More tests for positioning probability

--- src/test/java/de/neuenberger/serendipity/game/ResultTableTest.java
+++ src/test/java/de/neuenberger/serendipity/game/ResultTableTest.java
@@ -11,18 +11,23 @@ import de.neuenberger.serendipity.ProbabilityOutcome;
 import de.neuenberger.serendipity.SimpleProbabilityOutcome;
 
 public class ResultTableTest {
-	private final SimpleProbabilityOutcome t1 = new SimpleProbabilityOutcome("a");
-	private final SimpleProbabilityOutcome t2 = new SimpleProbabilityOutcome("b");
-	private final SimpleProbabilityOutcome t3 = new SimpleProbabilityOutcome("c");
-	private final SimpleProbabilityOutcome t4 = new SimpleProbabilityOutcome("d");
-	
+	private static final Offset<Double> OFFSET = Offset.offset(0.0000000001);
+	private static final String TEAM_A = "a";
+	private static final String TEAM_B = "b";
+	private static final String TEAM_C = "c";
+	private static final String TEAM_D = "d";
+	private final SimpleProbabilityOutcome t1 = new SimpleProbabilityOutcome(TEAM_A);
+	private final SimpleProbabilityOutcome t2 = new SimpleProbabilityOutcome(TEAM_B);
+	private final SimpleProbabilityOutcome t3 = new SimpleProbabilityOutcome(TEAM_C);
+	private final SimpleProbabilityOutcome t4 = new SimpleProbabilityOutcome(TEAM_D);
+
 	@Test
 	public void testPairing() {
-		List<Match> pairings = Match.createPairings(Arrays.asList(t1,t2,t3,t4), Consequence.values());
-		
+		List<Match> pairings = Match.createPairings(Arrays.asList(t1, t2, t3, t4), Consequence.values());
+
 		Assertions.assertThat(pairings).hasSize(6);
 		ResultTable resultTable = new ResultTable(pairings);
-		
+
 		List<ProbabilityOutcome> probabilityToBecomeFirst = resultTable.selectPosition(0);
 		Assertions.assertThat(probabilityToBecomeFirst).hasSize(4);
 		double probabilities = ProbabilityOutcome.sumAllProbabilities(probabilityToBecomeFirst);
@@ -32,14 +37,14 @@ public class ResultTableTest {
 			Assertions.assertThat(probabilityOutcome.getProbability()).isCloseTo(0.25, Offset.offset(0.00000001));
 		}
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testIllegalAccess() {
 		ResultTable resultTable = createResultTable();
 		resultTable.selectPosition(7);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testIllegalAccess2() {
 		ResultTable resultTable = createResultTable();
 		resultTable.selectPosition(-1);
@@ -79,6 +84,72 @@ public class ResultTableTest {
 		List<ProbabilityOutcome> list = resultTable.selectPosition(0);
 		for (ProbabilityOutcome probabilityOutcome : list) {
 			System.out.println(probabilityOutcome);
+		}
+	}
+
+	@Test
+	public void testAllHaveSamePoints() {
+
+		Match match1 = new Match(t1, t2, new MatchResult(Consequence.DRAW));
+		Match match2 = new Match(t3, t4, new MatchResult(Consequence.DRAW));
+
+		ResultTable resultTable = new ResultTable(Arrays.asList(match1, match2));
+		for (int i = 0; i < 4; i++) {
+			List<ProbabilityOutcome> list = resultTable.selectPosition(i);
+			double sumAllProbabilities = ProbabilityOutcome.sumAllProbabilities(list);
+			Assertions.assertThat(sumAllProbabilities).isCloseTo(1, OFFSET);
+			for (ProbabilityOutcome probabilityOutcome : list) {
+				Assertions.assertThat(probabilityOutcome.getProbability()).isCloseTo(0.25, OFFSET);
+			}
+		}
+	}
+
+	@Test
+	public void testClearWinner() {
+
+		Match match1 = new Match(t1, t2, new MatchResult(Consequence.WIN1));
+		Match match2 = new Match(t3, t4, new MatchResult(Consequence.DRAW));
+
+		ResultTable resultTable = new ResultTable(Arrays.asList(match1, match2));
+
+		for (int i = 0; i < 4; i++) {
+			List<ProbabilityOutcome> list = resultTable.selectPosition(i);
+			double sumAllProbabilities = ProbabilityOutcome.sumAllProbabilities(list);
+			Assertions.assertThat(sumAllProbabilities).isCloseTo(1, OFFSET);
+		}
+
+		{
+			List<ProbabilityOutcome> selectPosition1 = resultTable.selectPosition(0);
+			Assertions.assertThat(selectPosition1).hasSize(1);
+			ProbabilityOutcome outcomeTeamA = ProbabilityOutcome.getByTitle(selectPosition1, TEAM_A);
+			Assertions.assertThat(outcomeTeamA.getProbability()).isCloseTo(1.0, OFFSET); // sure
+																							// event;
+		}
+
+		{
+			List<ProbabilityOutcome> selectPosition4 = resultTable.selectPosition(3);
+			Assertions.assertThat(selectPosition4).hasSize(1);
+			ProbabilityOutcome outcomeTeamB = ProbabilityOutcome.getByTitle(selectPosition4, TEAM_B);
+			Assertions.assertThat(outcomeTeamB.getProbability()).isCloseTo(1.0, OFFSET); // sure
+																							// event;
+		}
+
+		{
+			List<ProbabilityOutcome> selectPosition2 = resultTable.selectPosition(1);
+			Assertions.assertThat(selectPosition2).hasSize(2);
+			ProbabilityOutcome outcomeTeamC2 = ProbabilityOutcome.getByTitle(selectPosition2, TEAM_C);
+			Assertions.assertThat(outcomeTeamC2.getProbability()).isCloseTo(0.5, OFFSET); // fifty-fifty-event;
+			ProbabilityOutcome outcomeTeamD2 = ProbabilityOutcome.getByTitle(selectPosition2, TEAM_D);
+			Assertions.assertThat(outcomeTeamD2.getProbability()).isCloseTo(0.5, OFFSET); // fifty-fifty-event;
+		}
+
+		{
+			List<ProbabilityOutcome> selectPosition3 = resultTable.selectPosition(2);
+			Assertions.assertThat(selectPosition3).hasSize(2);
+			ProbabilityOutcome outcomeTeamC2 = ProbabilityOutcome.getByTitle(selectPosition3, TEAM_C);
+			Assertions.assertThat(outcomeTeamC2.getProbability()).isCloseTo(0.5, OFFSET); // fifty-fifty-event;
+			ProbabilityOutcome outcomeTeamD2 = ProbabilityOutcome.getByTitle(selectPosition3, TEAM_D);
+			Assertions.assertThat(outcomeTeamD2.getProbability()).isCloseTo(0.5, OFFSET); // fifty-fifty-event;
 		}
 	}
 }
